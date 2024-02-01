96ddaeba4cfaa70c3d254323f894aa4d82b26680
Deanna Lepke <deannalepke@Deannas-MacBook-Pro-2.local>
2024-02-01 14:39:24 -0800

    login component skeletons, no login logic present

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
+import Register from "./Register";
+import Login from "./Login";
+import Landing from "./Landing";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* only show one of these, this still needs login logic to display conditionally */}
+        <Register badUsername={true} />
+        <Login loginFailed={true} />
+        <Landing />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ vite-project/src/Landing.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import "./App.css";
+
+function Landing() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the Reading Room!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
