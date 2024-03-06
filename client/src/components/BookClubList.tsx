5f3937c24aa8eeebe561be1de811def0ea24127f
Leanne Holmes <lholmes24@my.bcit.ca>
2024-03-06 12:25:23 -0800

    Styled Book Club List

--- client/src/components/BookClubList.tsx
+++ client/src/components/BookClubList.tsx
@@ -21,56 +21,61 @@ export default function BookClubList({ bookClubs }: Props) {
   return (
     <Grid divided="vertically">
       {bookClubs.map((bookclub) => (
-        <GridRow key={bookclub.id}>
+        <GridRow key={bookclub.id} stretched style={{ fontSize: "1.05em" }}>
           <GridColumn width={4}>
             <Image src={`/assets/${bookclub.category}.png`} />
           </GridColumn>
-          <GridColumn width={12}>
-            <Item>
-              <Item.Content>
-                <Item.Header>
-                  <Header size="medium">{bookclub.name}</Header>
-                </Item.Header>
-                <ItemMeta>
-                  {bookclub.members?.length}{" "}
-                  {bookclub.members?.length === 1 ? "Member" : "Members"}
-                </ItemMeta>
-                <Item.Description>
-                  <div>{bookclub.description}</div>
-                </Item.Description>
+          <GridColumn width={9}>
+            <GridRow style={{ height: "30%" }}>
+              <Header size="large">{bookclub.name}</Header>
+              {bookclub.members?.length}{" "}
+              {bookclub.members?.length === 1 ? "Member" : "Members"}
+            </GridRow>
+            <GridRow style={{ height: "50%", marginTop: "10px" }}>
+              <p>{bookclub.description}</p>
+              <p>
                 {bookclub.isOwner && (
                   <Item.Description>
-                    <Label basic color="green">
+                    <Label basic color="green" style={{ fontSize: "0.9em" }}>
                       You are the owner of this club
                     </Label>
                   </Item.Description>
                 )}
                 {bookclub.isMember && !bookclub.isOwner && (
                   <Item.Description>
-                    <Label basic color="orange">
+                    <Label basic color="orange" style={{ fontSize: "0.9em" }}>
                       You are a member of this club
                     </Label>
                   </Item.Description>
                 )}
-                <Item.Extra>
-                  <Label size="large" color="violet" as="a">
-                    <Icon name="book" />
-                    {bookclub.category}
-                  </Label>
-                  <Label size="large" color="teal" as="a">
-                    <Icon name="time" />
-                    {bookclub.readingPace} Pace
-                  </Label>
-                  <Button
-                    as={Link}
-                    to={`/bookclub/${bookclub.id}`}
-                    floated="right"
-                    content="View Club"
-                    color="blue"
-                  />
-                </Item.Extra>
-              </Item.Content>
-            </Item>
+              </p>
+            </GridRow>
+            <GridRow style={{ height: "20%" }}>
+              <Label
+                size="large"
+                as="a"
+                style={{ backgroundColor: "#06282d", color: "#fff" }}
+              >
+                <Icon name="book" />
+                {bookclub.category}
+              </Label>
+              <Label size="large" color="black" as="a">
+                <Icon name="time" />
+                {bookclub.readingPace} Pace
+              </Label>
+            </GridRow>
+          </GridColumn>
+          <GridColumn width={3}>
+            <GridRow style={{ height: "20%" }}>
+              <Button
+                as={Link}
+                to={`/bookclub/${bookclub.id}`}
+                floated="right"
+                icon
+              >
+                View Club <Icon name="chevron right" />
+              </Button>
+            </GridRow>
           </GridColumn>
         </GridRow>
       ))}
