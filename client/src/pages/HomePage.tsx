b0ace366874a3defc2dc9ccb281c93aba94582b7
Leanne Holmes <lholmes24@my.bcit.ca>
2024-03-07 01:50:56 -0800

    Fixed redirects for authorization

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 import { Container, Divider, Header } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
 import { useState } from "react";
 import RegisterForm from "../components/RegisterForm";
+import { useStore } from "../stores/store";
 
 function HomePage() {
+  const { userStore } = useStore();
+  const { isLoggedIn } = userStore;
   const [loginForm, setLoginForm] = useState(true);
 
   function handleClick() {
@@ -14,39 +17,48 @@ function HomePage() {
   }
 
   return (
-    <Container style={{ marginTop: "6em" }} textAlign="center">
-      <Header as="h1" className="playfair" style={{ marginBottom: "25px" }}>
-        Welcome to the Reading Room
-      </Header>
-      <Divider
-        style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}
-      />
-      <Container style={{ width: "60%", marginTop: "2em" }} textAlign="left">
-        {loginForm ? (
-          <>
-            <LoginForm />
-            <Container textAlign="center" style={{ marginTop: "1em" }}>
-              Don't have an account?{" "}
-              <Link onClick={handleClick} to={""}>
-                Click here
-              </Link>{" "}
-              to register
-            </Container>
-          </>
-        ) : (
-          <>
-            <RegisterForm />
-            <Container textAlign="center" style={{ marginTop: "1em" }}>
-              Already have an account?{" "}
-              <Link onClick={handleClick} to={""}>
-                Click here
-              </Link>{" "}
-              to login
-            </Container>
-          </>
-        )}
-      </Container>
-    </Container>
+    <>
+      {isLoggedIn ? (
+        <Navigate to="/bookclubs" />
+      ) : (
+        <Container style={{ marginTop: "6em" }} textAlign="center">
+          <Header as="h1" className="playfair" style={{ marginBottom: "25px" }}>
+            Welcome to the Reading Room
+          </Header>
+          <Divider
+            style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}
+          />
+          <Container
+            style={{ width: "60%", marginTop: "2em" }}
+            textAlign="left"
+          >
+            {loginForm ? (
+              <>
+                <LoginForm />
+                <Container textAlign="center" style={{ marginTop: "1em" }}>
+                  Don't have an account?{" "}
+                  <Link onClick={handleClick} to={""}>
+                    Click here
+                  </Link>{" "}
+                  to register
+                </Container>
+              </>
+            ) : (
+              <>
+                <RegisterForm />
+                <Container textAlign="center" style={{ marginTop: "1em" }}>
+                  Already have an account?{" "}
+                  <Link onClick={handleClick} to={""}>
+                    Click here
+                  </Link>{" "}
+                  to login
+                </Container>
+              </>
+            )}
+          </Container>
+        </Container>
+      )}
+    </>
   );
 }
 
