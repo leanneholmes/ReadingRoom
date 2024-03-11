c9f8de9d3c760282bf5782791cac3a69e509ab32
Leanne Holmes <lholmes24@my.bcit.ca>
2024-03-11 15:03:40 -0700

    Styled homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,11 @@
-import { Container, Divider, Header } from "semantic-ui-react";
+import {
+  Container,
+  Divider,
+  Grid,
+  GridColumn,
+  GridRow,
+  Header,
+} from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { Link, Navigate } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
@@ -21,42 +28,83 @@ function HomePage() {
       {isLoggedIn ? (
         <Navigate to="/bookclubs" />
       ) : (
-        <Container style={{ marginTop: "6em" }} textAlign="center">
-          <Header as="h1" className="playfair" style={{ marginBottom: "25px" }}>
-            Welcome to the Reading Room
-          </Header>
-          <Divider
-            style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}
-          />
+        <div>
           <Container
-            style={{ width: "60%", marginTop: "2em" }}
-            textAlign="left"
+            style={{
+              marginTop: "6em",
+              paddingRight: "8em",
+              paddingLeft: "8em",
+            }}
+            textAlign="center"
           >
-            {loginForm ? (
-              <>
-                <LoginForm />
-                <Container textAlign="center" style={{ marginTop: "1em" }}>
-                  Don't have an account?{" "}
-                  <Link onClick={handleClick} to={""}>
-                    Click here
-                  </Link>{" "}
-                  to register
-                </Container>
-              </>
-            ) : (
-              <>
-                <RegisterForm />
-                <Container textAlign="center" style={{ marginTop: "1em" }}>
-                  Already have an account?{" "}
-                  <Link onClick={handleClick} to={""}>
-                    Click here
-                  </Link>{" "}
-                  to login
-                </Container>
-              </>
-            )}
+            <Grid celled style={{ borderRadius: "10px" }}>
+              <GridRow style={{ borderRadius: "10px" }}>
+                <GridColumn
+                  width={9}
+                  style={{
+                    marginTop: "7em",
+                    marginBottom: "7em",
+                  }}
+                >
+                  <Header
+                    as="h1"
+                    className="playfair"
+                    style={{ marginBottom: "25px" }}
+                  >
+                    Welcome to Reading Room
+                  </Header>
+                  <Divider
+                    style={{
+                      width: "50%",
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                    }}
+                  />
+                  <Container
+                    style={{
+                      width: "70%",
+                      marginTop: "2em",
+                      marginBottom: "2em",
+                      height: "20em",
+                    }}
+                    textAlign="left"
+                  >
+                    {loginForm ? (
+                      <>
+                        <LoginForm />
+                        <Container
+                          textAlign="center"
+                          style={{ marginTop: "1em" }}
+                        >
+                          Don't have an account?{" "}
+                          <Link onClick={handleClick} to={""}>
+                            Click here
+                          </Link>{" "}
+                          to register
+                        </Container>
+                      </>
+                    ) : (
+                      <>
+                        <RegisterForm />
+                        <Container
+                          textAlign="center"
+                          style={{ marginTop: "1em" }}
+                        >
+                          Already have an account?{" "}
+                          <Link onClick={handleClick} to={""}>
+                            Click here
+                          </Link>{" "}
+                          to login
+                        </Container>
+                      </>
+                    )}
+                  </Container>
+                </GridColumn>
+                <GridColumn width={7} className="login-image"></GridColumn>
+              </GridRow>
+            </Grid>
           </Container>
-        </Container>
+        </div>
       )}
     </>
   );
