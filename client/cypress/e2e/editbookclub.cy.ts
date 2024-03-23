6cec8394611e8338ae30b8a089bdc1fa13f0942d
dlepke <deannalepke@gmail.com>
2024-03-22 23:57:38 -0700

    updated test cases for login, register, create and edit clubs

--- client/cypress/e2e/editbookclub.cy.ts
+++ client/cypress/e2e/editbookclub.cy.ts
@@ -1,5 +1,5 @@
 describe("editing a book club", () => {
-  it("edits a book club description", () => {
+  it("edits a book club", () => {
     //login first
     cy.intercept("POST", "http://localhost:5000/api/account/login").as(
       "loginRequest"
@@ -32,25 +32,55 @@ describe("editing a book club", () => {
     // type in something in club description
     // click submit
 
+    cy.get("a[type=pageItem]").contains("2").click();
+
     cy.contains(".segment", "You are the owner of this club").within(() => {
       cy.get("a").contains("View Club").click();
     });
 
-    // check original
-    cy.get("h4")
-      .contains("Club Description")
-      .parent()
-      .within(() => {
-        cy.contains("A club for mystery enthusiasts");
-      });
-
     cy.get("a[id=edit]").click();
 
-    cy.get("textarea[name=description]").type("A new club 22 description");
+    const clubname = "a new club " + Math.floor(Math.random() * 50);
+
+    cy.get("input[name=name]").clear();
+
+    cy.get("input[name=name]").type(clubname);
+
+    cy.get("textarea[name=description").clear();
+
+    cy.get("textarea[name=description]").type(
+      "A new " + clubname + " description"
+    );
 
     cy.get("input[name=meetingLink]").clear();
 
     cy.get("input[name=meetingLink]").type("http://www.zoom.ca");
+
+    cy.get("input[name=nextMeeting]").type("April 18, 2025 1:30AM");
+
+    cy.get("h4").contains("Genre").click();
+    cy.get("h4")
+      .contains("Genre")
+      .parents(".row")
+      .find(".ui.selection.dropdown")
+      .click();
+    cy.get("span").contains("Romance").click();
+
+    // cy.get("h4").contains("Reading Pace").siblings("div[role=listbox]").click();
+    cy.get("h4")
+      .contains("Reading Pace")
+      .parents(".row")
+      .find(".ui.selection.dropdown")
+      .click();
+    cy.get("span").contains("Fast").click();
+
+    cy.get("input[name=currentBook").clear();
+
+    cy.get("input[name=currentBook").type(clubname + "'s current book");
+
+    cy.get("input[name=currentBookAuthor").clear();
+
+    cy.get("input[name=currentBookAuthor").type(clubname + "'s fave author");
 
     cy.get("button[id=submit]").click();
 
@@ -59,7 +89,108 @@ describe("editing a book club", () => {
       .contains("Club Description")
       .parent()
       .within(() => {
-        cy.contains("A new club 22 description");
+        cy.contains("A new " + clubname + " description");
       });
+  });
+
+  it("tries to change name to one that already exists", () => {
+    //login first
+    cy.intercept("POST", "http://localhost:5000/api/account/login").as(
+      "loginRequest"
+    );
+
+    cy.intercept(
+      "GET",
+      "http://localhost:5000/api/bookclubs?pageNumber=1&pagesize=5&all=true"
+    ).as("bookclubsLoad");
+
+    cy.intercept("GET", "http://localhost:5000/api/account").as("accountLoad");
+
+    cy.visit("localhost:3000");
+
+    cy.get("input[name=email]").type("deanna@test.com");
+
+    cy.get("input[name=password]").type("Pa$$w0rd");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
+
+    cy.wait("@bookclubsLoad").its("response.statusCode").should("eq", 200);
+
+    cy.wait("@accountLoad").its("response.statusCode").should("eq", 200);
+
+    // grab a club that you own
+    // click view club
+    // click edit
+    // type in something in club description
+    // click submit
+
+    cy.get("a[type=pageItem]").contains("2").click();
+
+    cy.contains(".segment", "You are the owner of this club").within(() => {
+      cy.get("a").contains("View Club").click();
+    });
+
+    cy.get("a[id=edit]").click();
+
+    cy.get("input[name=name]").clear();
+
+    cy.get("input[name=name]").type("Classic Literature Society");
+
+    cy.get("button[id=submit]").should("be.disabled");
+  });
+
+  it("edits name but cancels", () => {
+    //login first
+    cy.intercept("POST", "http://localhost:5000/api/account/login").as(
+      "loginRequest"
+    );
+
+    cy.intercept(
+      "GET",
+      "http://localhost:5000/api/bookclubs?pageNumber=1&pagesize=5&all=true"
+    ).as("bookclubsLoad");
+
+    cy.intercept("GET", "http://localhost:5000/api/account").as("accountLoad");
+
+    cy.visit("localhost:3000");
+
+    cy.get("input[name=email]").type("deanna@test.com");
+
+    cy.get("input[name=password]").type("Pa$$w0rd");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
+
+    cy.wait("@bookclubsLoad").its("response.statusCode").should("eq", 200);
+
+    cy.wait("@accountLoad").its("response.statusCode").should("eq", 200);
+
+    // grab a club that you own
+    // click view club
+    // click edit
+    // type in something in club description
+    // click submit
+
+    cy.get("a[type=pageItem]").contains("2").click();
+
+    cy.contains(".segment", "You are the owner of this club").within(() => {
+      cy.get("a").contains("View Club").click();
+    });
+
+    cy.get("a[id=edit]").click();
+
+    const clubname = "a new club " + Math.floor(Math.random() * 50);
+
+    cy.get("input[name=name]").clear();
+
+    cy.get("input[name=name]").type(clubname);
+
+    cy.get("button[id=cancel]").click();
+
+    // confirm replaced with new
+    cy.get("h2").should("not.include.text", clubname);
   });
 });
--- client/cypress/e2e/register.cy.ts
+++ client/cypress/e2e/register.cy.ts
@@ -34,6 +34,68 @@ describe("register flow", () => {
     cy.wait("@accountLoad").its("response.statusCode").should("eq", 200);
   });
 
+  it("enters incorrect password format", () => {
+    cy.intercept("POST", "http://localhost:5000/api/account/register").as(
+      "registerRequest"
+    );
+
+    cy.intercept(
+      "GET",
+      "http://localhost:5000/api/bookclubs?pageNumber=1&pagesize=5&all=true"
+    ).as("bookclubsLoad");
+
+    cy.intercept("GET", "http://localhost:5000/api/account").as("accountLoad");
+
+    cy.visit("localhost:3000");
+
+    cy.get("a").contains("Click here").click();
+
+    cy.get("input[name=displayName]").type("New user");
+
+    const username = "newuser" + Math.random() * 20;
+
+    cy.get("input[name=username]").type(username);
+
+    cy.get("input[name=email]").type(username + "@test.com");
+
+    cy.get("input[name=password]").type("1234");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@registerRequest").its("response.statusCode").should("eq", 400);
+  });
+
+  it("enters incorrect email format", () => {
+    cy.intercept("POST", "http://localhost:5000/api/account/register").as(
+      "registerRequest"
+    );
+
+    cy.intercept(
+      "GET",
+      "http://localhost:5000/api/bookclubs?pageNumber=1&pagesize=5&all=true"
+    ).as("bookclubsLoad");
+
+    cy.intercept("GET", "http://localhost:5000/api/account").as("accountLoad");
+
+    cy.visit("localhost:3000");
+
+    cy.get("a").contains("Click here").click();
+
+    cy.get("input[name=displayName]").type("New user");
+
+    const username = "newuser" + Math.random() * 20;
+
+    cy.get("input[name=username]").type(username);
+
+    cy.get("input[name=email]").type(username);
+
+    cy.get("input[name=password]").type("Pa$$w0rd!");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@registerRequest").its("response.statusCode").should("eq", 400);
+  });
+
   it("enters existing username and password", () => {
     cy.intercept("POST", "http://localhost:5000/api/account/register").as(
       "registerRequest"
@@ -46,6 +108,54 @@ describe("register flow", () => {
     cy.get("input[name=displayName]").type("Deanna");
 
     cy.get("input[name=username]").type("deanna");
+
+    cy.get("input[name=email]").type("deanna@test.com");
+
+    cy.get("input[name=password]").type("Pa$$w0rd");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@registerRequest").its("response.statusCode").should("eq", 400);
+  });
+
+  it("enters existing username but different email", () => {
+    cy.intercept("POST", "http://localhost:5000/api/account/register").as(
+      "registerRequest"
+    );
+
+    cy.visit("localhost:3000");
+
+    cy.get("a").contains("Click here").click();
+
+    cy.get("input[name=displayName]").type("Deanna");
+
+    cy.get("input[name=username]").type("deanna");
+
+    const email = "newuser" + Math.random() * 20 + "@test.com";
+
+    cy.get("input[name=email]").type(email);
+
+    cy.get("input[name=password]").type("Pa$$w0rd");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@registerRequest").its("response.statusCode").should("eq", 400);
+  });
+
+  it("enters existing email but different username", () => {
+    cy.intercept("POST", "http://localhost:5000/api/account/register").as(
+      "registerRequest"
+    );
+
+    cy.visit("localhost:3000");
+
+    cy.get("a").contains("Click here").click();
+
+    cy.get("input[name=displayName]").type("Deanna");
+
+    const username = "deanna" + Math.random() * 20;
+
+    cy.get("input[name=username]").type(username);
 
     cy.get("input[name=email]").type("deanna@test.com");
 
