fe81c630ea87f60dfd5b4755d9a55321a46c0e3b
B-Rabbit7 <snouri3@my.bcit.ca>
2024-04-01 17:03:42 -0700

    fixed white space error

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
-            return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username })); // Inserted space after Details.Query
         }
 
         [HttpPut]
@@ -20,7 +20,33 @@ namespace API.Controllers
         [HttpGet("{username}/bookclubs")]
         public async Task<IActionResult> GetUserBookClubs(string username)
         {
-            return HandleResult(await Mediator.Send(new ListBookClubs.Query{ Username = username }));
+            return HandleResult(await Mediator.Send(new ListBookClubs.Query { Username = username })); // Inserted space after ListBookClubs.Query
+        }
+    }
+}
+using Application.Profiles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProfilesController : BaseApiController
+    {
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+
+        [HttpGet("{username}/bookclubs")]
+        public async Task<IActionResult> GetUserBookClubs(string username)
+        {
+            return HandleResult(await Mediator.Send(new ListBookClubs.Query { Username = username }));
         }
     }
 }
